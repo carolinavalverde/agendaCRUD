f3d9b97536903064ac78a6b6f1e1c9d757adf28f
Caro Valverde <carolina.valverde255@gmail.com>
2023-12-16 11:06:42 -0300

    agrego validaciones

---
+++ README.md
@@ -0,0 +1,11 @@
+CRUD Agenda con javascript
+Este proyecto esta realizado con javascript y cumple todas las operaciones del CRUD y ademas guarda los contactos de la agenda en localstorage.
+
+Demo
+aqui agregar el link
+
+Librerias 📚
+Bootstrap v5.3.2
+SweetAlert v2
+Autores
+Arias Emilse
--- js/detalleContacto.js
+++ js/detalleContacto.js
@@ -1,13 +1,12 @@
-console.log(window.location.search)
+console.log(window.location.search);
 
-const parametroURL = new URLSearchParams(window.location.search)
-const idContacto = parametroURL.get(`id`);
+const parametroURL = new URLSearchParams(window.location.search);
+const idContacto = parametroURL.get("id");
 
-console.log(idContacto)
+console.log(idContacto);
 
-//1. traer los datos del localStorage
-//get item Array
+//1- traer los datos del localstorage
 
-//2. buscar el objeto que tiene el idContacto en el array agenda
+//2- buscar el objeto que tiene el idContacto en el array agenda.
 
-//3. dibujar la card horizontal con los datos del objeto encontrado
+//3- dibujar la card horizontal con los datos del objeto encontrado
